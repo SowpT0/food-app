4cba6e60b33e0c672990c32b67576f16fc2ce789
Shaun <shaunteoh1995@gmail.com>
2023-12-15 09:58:08 +0800

    3rdcommit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { app } from './firebase';
+
 
 import App from './App.jsx';
 
